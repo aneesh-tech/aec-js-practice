74599710b79204597af4b714b2515837cca4582b
Aneesh Pajimannu <146318529+aneesh-tech@users.noreply.github.com>
2026-02-24 06:33:27 +0530

    Initialize user object and update email

---
+++ q6_objects.js
@@ -0,0 +1,13 @@
+let user = {
+  name: "Aneesh",
+  email: "aneesh@gmail.com",
+  password: "1234",
+  age: 20
+};
+
+console.log(user.name);
+
+user.email = "aneesh@newmail.com";
+user.isAdmin = false;
+
+console.log(user);
