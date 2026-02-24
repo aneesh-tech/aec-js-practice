e737a59b0ecd369ef92e1b65cd34c64b04b12ac7
Aneesh Pajimannu <146318529+aneesh-tech@users.noreply.github.com>
2026-02-24 06:32:45 +0530

    Add loops for numbers, evens, and multiplication table

---
+++ q4_loops.js
@@ -0,0 +1,12 @@
+console.log("Numbers 1 to 20:");
+for (let i = 1; i <= 20; i++) console.log(i);
+
+console.log("Even numbers:");
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0) console.log(i);
+}
+
+console.log("Multiplication table of 5:");
+for (let i = 1; i <= 10; i++) {
+  console.log(`5 x ${i} = ${5 * i}`);
+}
